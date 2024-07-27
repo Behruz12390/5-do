3c39fa4297e87f868d64e5be7823487181e57c69
Behruz12390 <168063527+Behruz12390@users.noreply.github.com>
2024-07-27 12:29:09 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// let a = document.createElement('p')
+// let b = document.querySelector('.wrap')
+// a.textContent = 'wveduetfuyjgestryftdg66yhwyrwgt6d3tyf46e'
+// b.append(a)
+
+let a = prompt('ism kiriting')
+let h1 = document.querySelector('.te')
+h1.innerHTML = a
